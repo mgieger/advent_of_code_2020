f74c712f4f292049977f371493afd93456c07fef
Matthew Gieger <mrgieger@gmail.com>
2020-12-06 21:44:45 -0800

    Update part one code to handle additions for part two

--- lib/mix/tasks/day_three.ex
+++ lib/mix/tasks/day_three.ex
@@ -3,25 +3,45 @@ defmodule Mix.Tasks.DayThree do
 
   @shortdoc "Finds the number of trees the toboggan encounters"
 
-  def run(_args) do
+  @part_one_coordinates [{0, 0}]
+  @part_two_coordinates []
+
+  def run(args) do
+    {parsed_args, _, _} = OptionParser.parse(args, strict: [part_two: :boolean])
+
     File.read!("data_files/day_three.txt")
     |> parse_input()
-    |> tobaggan_down_the_hill()
+    |> add_coordinate_set(parsed_args)
+    |> toboggan_down_the_hill()
     |> IO.inspect()
   end
 
   def parse_input(data) do
     data
     |> String.split("\n", trim: true)
-    |> Enum.drop(1)
   end
 
-  def tobaggan_down_the_hill(data) do
-    {_index, trees_encountered} =
-      data
-      |> Enum.reduce({0, 0}, &determine_spot_and_increment_count/2)
+  def add_coordinate_set(data, []) do
+    {data, @part_one_coordinates}
+  end
+
+  def add_coordinate_set(data, [part_two: true]) do
+    {data, @part_two_coordinates}
+  end
+
+  def toboggan_down_the_hill({data, coordinate_set}) do
+    trees_encountered =
+      coordinate_set
+      |> Enum.map(&toboggan_run(&1, data))
+      |> Enum.reduce(1, fn({_index, trees_encountered}, acc) -> acc * trees_encountered end)
 
     trees_encountered
+  end
+
+  def toboggan_run({row, column}, data) do
+    data
+    |> Enum.drop(row + 1)
+    |> Enum.reduce({column, 0}, &determine_spot_and_increment_count/2)
   end
 
   def determine_spot_and_increment_count(row_string, {index, tree_count}) do
--- solution_explanations/day_three.md
+++ solution_explanations/day_three.md
@@ -22,3 +22,9 @@ Count the number of "#" spaces you would land on.
 ```mix day_three```
 
 The first row is removed since we know our beginning index and tree count are 0 and 0. Then a reduce function with a two element tuple accumulator is used to maintain track of the index and tree count.
+
+## Part 2
+Given a set of coordinates representing different starting points, traverse the map and count the number of trees encountered.
+
+### Solution
+```mix day_three --part-two```
