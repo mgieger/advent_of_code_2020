e2e461745834d59e5e0e1bc9cab03c3cea1b5842
Matthew Gieger <mrgieger@gmail.com>
2020-12-07 22:35:03 -0800

    Part two code complete

--- lib/mix/tasks/day_four.ex
+++ lib/mix/tasks/day_four.ex
@@ -3,14 +3,15 @@ defmodule Mix.Tasks.DayFour do
 
   @shortdoc "Counts the number of valid passports"
 
-  # @required_keys ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
-  # @optional_keys ["cid"]
+  @eye_colors ~w(amb blu brn gry grn hzl oth)
 
-  def run(_args) do
+  def run(args) do
+    {parsed_args, _, _} = OptionParser.parse(args, strict: [part_two: :boolean])
+
     File.read!("data_files/day_four.txt")
     |> separate_passports()
     |> convert_passports_to_maps()
-    |> count_valid_passports()
+    |> count_valid_passports(parsed_args)
     |> IO.inspect()
   end
 
@@ -34,9 +35,47 @@ defmodule Mix.Tasks.DayFour do
       end)
   end
 
-  defp count_valid_passports(passports) do
-    passports
-    |> Enum.reduce(0, &validate_passport/2)
+  defp count_valid_passports(passports, parsed_args) do
+    {valid_passports, _} =
+      passports
+      |> Enum.reduce({0, parsed_args}, &validate_passport/2)
+
+    valid_passports
+  end
+
+  defp validate_passport(
+    %{
+      "byr" => byr,
+      "ecl" => ecl,
+      "eyr" => eyr,
+      "hcl" => hcl,
+      "hgt" => hgt,
+      "iyr" => iyr,
+      "pid" => pid
+    },
+    {acc, [part_two: true]}
+  ) do
+    byr = String.to_integer(byr)
+    iyr = String.to_integer(iyr)
+    eyr = String.to_integer(eyr)
+    valid_hgt = height_valid?(hgt, Regex.match?(~r/^(\d+)(cm|in)$/, hgt))
+    valid_hcl = Regex.match?(~r/^#[a-f0-9]{6}$/, hcl)
+    valid_ecl = Enum.any?(@eye_colors, fn color -> color == ecl end)
+    valid_pid = Regex.match?(~r/^\d{9}$/, pid)
+
+    with(
+      true <- 1920 <= byr and byr <= 2002,
+      true <- 2010 <= iyr and iyr <= 2020,
+      true <- 2020 <= eyr and eyr <= 2030,
+      true <- valid_hgt,
+      true <- valid_hcl,
+      true <- valid_ecl,
+      true <- valid_pid
+    ) do
+      {acc + 1, [part_two: true]}
+    else
+      _ -> {acc, [part_two: true]}
+    end
   end
 
   defp validate_passport(
@@ -49,11 +88,26 @@ defmodule Mix.Tasks.DayFour do
       "iyr" => _iyr,
       "pid" => _pid
     },
-    acc) do
-      acc + 1
+    {acc, []}
+    ) do
+      {acc + 1, []}
   end
 
   defp validate_passport(_invalid_passport, acc) do
     acc
+  end
+
+  defp height_valid?(hgt, true) do
+    [_hgt, value, unit] = List.flatten(Regex.scan(~r/^(\d+)(cm|in)$/, hgt))
+    value = String.to_integer(value)
+
+    case unit do
+      "cm" -> 150 <= value and value <= 194
+      "in" -> 59 <= value and value <= 76
+    end
+  end
+
+  defp height_valid?(_hgt, false) do
+    false
   end
 end
--- solution_explanations/day_four.md
+++ solution_explanations/day_four.md
@@ -33,5 +33,18 @@ Optional fields:
 ### Solution
 ```mix day_four```
 
-The first problem to solve is getting each passport into one row so we can then process the passports. Splitting the data input on `\n\n` ensures we have a list of values where each value is all of the information for one passport. Replacing the remaining `\n` characters merges all the passport information into one row.
+This solution uses pattern matching in a function header to validate passwords.
+
+## Part 2
+Add additional validation:
+- byr (Birth Year) - four digits; at least 1920 and at most 2002.
+- iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+- eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+- hgt (Height) - a number followed by either cm or in:
+- If cm, the number must be at least 150 and at most 193.
+- If in, the number must be at least 59 and at most 76.
+- hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+- ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+- pid (Passport ID) - a nine-digit number, including leading zeroes.
+- cid (Country ID) - ignored, missing or not.
 
